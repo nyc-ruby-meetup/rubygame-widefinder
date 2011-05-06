18363e892c40d7e481ce3f06e4c776a03bf0b536
Ian Sheridan <ian.sheridan@gmail.com>
2011-05-06 01:34:24 -0400

    removed unneeded instance variable, moved all File interaction to one method and replaced split with regex

--- widefinder.rb
+++ widefinder.rb
@@ -3,13 +3,12 @@ class WideFinder
   attr_accessor :filename
   
   def initialize(logfile)
-    @logfile = File.open(logfile,"r").read.split("\n")
-    @counter = {}
+    @logfile = logfile
   end
 
   def results(num)
-    add_to_hash
-    res1 = @counter.sort_by{|k,v| -v}
+    hash = file_to_hash
+    res1 = hash.sort_by{|k,v| -v}
     res2 = []
     res1[0..(num-1)].each{|o|
       res2 << "#{o[1]}: #{o[0]}"
@@ -17,12 +16,14 @@ class WideFinder
     res2
   end
 
-  def add_to_hash
-    @logfile.each{|l|
-      file = l.split(" ")[6].split("?")[0]
-      @counter[file] = 0 unless @counter.key? file
-      @counter[file] = @counter[file] + 1
+  def file_to_hash
+    output = Hash.new
+    File.open(@logfile,"r").each_line{|line|
+      file = line[/(GET|POST) ([^? ]*)/,2]
+      output[file] = 0 unless output.key? file
+      output[file] = output[file] + 1
     }
+    output
   end
 
 end
