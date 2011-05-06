f4b756e5e53927f103361ae305641b73c5fb7639
Julian Burgess <aubergene@gmail.com>
2011-05-05 20:20:54 -0400

    working version

--- widefinder.rb
+++ widefinder.rb
@@ -1,11 +1,25 @@
 class WideFinder
-  
+
   attr_accessor :filename
-  
-  def initialize
+
+  def initialize(filename)
+    @filename = filename
+    @hit_count = Hash.new(0)
+    parse_log
   end
 
-  def results
+  def parse_log
+    File.open(@filename) do |file|
+      while line = file.gets do
+        page = line[/GET ([^? ]+)/,1]
+
+        @hit_count[page] += 1
+      end
+    end
+  end
+
+  def results(num)
+    @hit_count.sort{ |a,b| b[1] <=> a[1] }.take(num).map{ |x| x.reverse.join(': ')  }
   end
 
 end
