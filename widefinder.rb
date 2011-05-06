c20b7c601f1acd0134cbd037dab35a0ac0c25cc6
Ian Sheridan <ian.sheridan@gmail.com>
2011-05-06 01:50:09 -0400

    a little more refactoring using enumerable's "take" method thanks to aubergene

--- widefinder.rb
+++ widefinder.rb
@@ -3,22 +3,16 @@ class WideFinder
   attr_accessor :filename
   
   def initialize(logfile)
-    @logfile = logfile
+    @filename = logfile
   end
 
   def results(num)
-    hash = file_to_hash
-    res1 = hash.sort_by{|k,v| -v}
-    res2 = []
-    res1[0..(num-1)].each{|o|
-      res2 << "#{o[1]}: #{o[0]}"
-    }
-    res2
+    file_to_hash.sort_by{|k,v| -v }.take(num).map{|x| x.reverse.join(": ") }
   end
 
   def file_to_hash
     output = Hash.new
-    File.open(@logfile,"r").each_line{|line|
+    File.open(@filename,"r").each_line{|line|
       file = line[/(GET|POST) ([^? ]*)/,2]
       output[file] = 0 unless output.key? file
       output[file] = output[file] + 1
