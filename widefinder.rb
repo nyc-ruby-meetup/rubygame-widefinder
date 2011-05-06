e0a85a920227b1c93968f5b8a1b78267e21be355
Ian Sheridan <ian.sheridan@gmail.com>
2011-05-05 20:44:38 -0400

    Ians and Abels awesome code

--- widefinder.rb
+++ widefinder.rb
@@ -2,10 +2,27 @@ class WideFinder
   
   attr_accessor :filename
   
-  def initialize
+  def initialize(logfile)
+    @logfile = File.open(logfile,"r").read.split("\n")
+    @counter = {}
   end
 
-  def results
+  def results(num)
+    add_to_hash
+    res1 = @counter.sort_by{|k,v| -v}
+    res2 = []
+    res1[0..(num-1)].each{|o|
+      res2 << "#{o[1]}: #{o[0]}"
+    }
+    res2
+  end
+
+  def add_to_hash
+    @logfile.each{|l|
+      file = l.split(" ")[6].split("?")[0]
+      @counter[file] = 0 unless @counter.key? file
+      @counter[file] = @counter[file] + 1
+    }
   end
 
 end
