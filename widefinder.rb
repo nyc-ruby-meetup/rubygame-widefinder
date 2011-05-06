3039f70816e673fb5373cc66687b20fbb5f5529a
Moudy <moudy.elkammash@gmail.com>
2011-05-05 20:46:49 -0400

    working

--- widefinder.rb
+++ widefinder.rb
@@ -1,11 +1,24 @@
 class WideFinder
-  
+
   attr_accessor :filename
-  
-  def initialize
+
+  def initialize(filename)
+    @filename = filename
   end
 
-  def results
+  def results(count)
+    store = {}
+    File.open(@filename, 'r') do |f|
+      f.each do |line|
+        line =~ %r{"\w+\s([^\s]*?)(\?|\s)}
+        store[$1] ||= 0
+        store[$1] += 1
+      end
+    end
+
+    store.sort { |a,b| b[1] <=> a[1] }[0...count].collect do |page, count|
+      "#{count}: #{page}"
+    end
   end
 
 end
